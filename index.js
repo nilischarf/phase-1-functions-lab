2724a1e2c94f9aa60ddc68703916578a6648226b
Nili Scharf <nilischarf@gmail.com>
2024-10-31 11:41:55 +0200

    complete functions lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickup){
+    const hq = 42
+    return Math.abs(pickup - hq)
+}
+
+
+function distanceFromHqInFeet(pickup){
+    const blocks = distanceFromHqInBlocks(pickup)
+    return blocks * 264
+}
+
+
+function distanceTravelledInFeet(start, destination){
+    const distance = Math.abs(destination-start) * 264
+    return distance
+}
+
+
+function calculatesFarePrice(start, destination){
+    const distance = Math.abs(destination-start) * 264
+    if (distance <= 400){
+        return 0
+    }
+        else if (distance <= 2000){
+            return 0.02 * (distance-400)
+        }
+        else if (distance < 2500){
+            return 25
+        }
+        else if (distance >= 2500){
+            return 'cannot travel that far'
+        }
+}
